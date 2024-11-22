f6de831d24fdf56cd213fba2afeccd1b403e6446
cristiano.nogueira@gruporas.com.br <cristiano.nogueira@gruporas.com.br>
2024-11-22 17:13:14 -0300

    primeiro commit

---
+++ src/main/java/com/greenbeans/school_transportation/SchoolTransportationApplication.java
@@ -0,0 +1,13 @@
+package com.greenbeans.school_transportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTransportationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolTransportationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenbeans/school_transportation/SchoolTransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenbeans.school_transportation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolTransportationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
